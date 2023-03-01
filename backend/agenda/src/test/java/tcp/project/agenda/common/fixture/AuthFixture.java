5e1f8f9a2cb65fe6299b147a42dc919a00cce186
wlsh44 <k87913j@gmail.com>
2023-03-01 23:42:16 +0900

    test: jwt 관련 기능 테스트 코드 추가

---
+++ backend/agenda/src/test/java/tcp/project/agenda/common/fixture/AuthFixture.java
@@ -0,0 +1,6 @@
+package tcp.project.agenda.common.fixture;
+
+public class AuthFixture {
+
+    public static final String SECRET_KEY = "secret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-keysecret-key";
+}
