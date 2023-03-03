f25e9bfc589d88ac766846fea4c174062680dff2
wlsh44 <k87913j@gmail.com>
2023-03-03 19:30:04 +0900

    feat: 인증 토큰 추출 및 검증 인터셉터 추가

---
+++ backend/agenda/src/main/java/tcp/project/agenda/auth/exception/AuthExtractException.java
@@ -0,0 +1,13 @@
+package tcp.project.agenda.auth.exception;
+
+import org.springframework.http.HttpStatus;
+import tcp.project.agenda.common.exception.AgendaException;
+
+public class AuthExtractException extends AgendaException {
+
+    public static final String ERROR_MSG = "올바르지 않은 인증 헤더입니다.";
+
+    public AuthExtractException() {
+        super(ERROR_MSG, HttpStatus.BAD_REQUEST);
+    }
+}
