1e63501e099396f63c6dad75eeee7e3a4b762de2
wlsh44 <k87913j@gmail.com>
2023-03-03 19:37:29 +0900

    fix: 토큰 추출 실패 시 예외 코드 수정

--- backend/agenda/src/main/java/tcp/project/agenda/auth/exception/AuthExtractException.java
+++ backend/agenda/src/main/java/tcp/project/agenda/auth/exception/AuthExtractException.java
@@ -8,6 +8,6 @@ public class AuthExtractException extends AgendaException {
     public static final String ERROR_MSG = "올바르지 않은 인증 헤더입니다.";
 
     public AuthExtractException() {
-        super(ERROR_MSG, HttpStatus.BAD_REQUEST);
+        super(ERROR_MSG, HttpStatus.UNAUTHORIZED);
     }
 }
