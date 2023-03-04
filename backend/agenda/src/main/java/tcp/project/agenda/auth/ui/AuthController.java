b6c22cb9f2d13e3b4452aee28235e518e234cc4f
wlsh44 <k87913j@gmail.com>
2023-03-04 21:15:14 +0900

    fix: request body 어노테이션 추가

--- backend/agenda/src/main/java/tcp/project/agenda/auth/ui/AuthController.java
+++ backend/agenda/src/main/java/tcp/project/agenda/auth/ui/AuthController.java
@@ -3,6 +3,7 @@ package tcp.project.agenda.auth.ui;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tcp.project.agenda.auth.application.AuthService;
@@ -17,7 +18,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(LoginRequest request) {
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
         TokenResponse response = authService.login(request);
         return ResponseEntity.ok(response);
     }
