6953dd457c0a806c3b795bacc3d571a5beea542d
wlsh44 <k87913j@gmail.com>
2023-03-04 21:15:41 +0900

    fix: 권한 체크 인터셉터 삭제

--- backend/agenda/src/main/java/tcp/project/agenda/auth/config/AuthConfig.java
+++ backend/agenda/src/main/java/tcp/project/agenda/auth/config/AuthConfig.java
@@ -6,8 +6,7 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tcp.project.agenda.auth.ui.AuthenticationArgumentResolver;
-import tcp.project.agenda.auth.ui.interceptor.BasicAuthInterceptor;
-import tcp.project.agenda.auth.ui.interceptor.RepresentationAuthInterceptor;
+import tcp.project.agenda.auth.ui.BasicAuthInterceptor;
 
 import java.util.List;
 
@@ -16,12 +15,12 @@ import java.util.List;
 public class AuthConfig implements WebMvcConfigurer {
 
     private final BasicAuthInterceptor basicAuthInterceptor;
-    private final RepresentationAuthInterceptor representationAuthInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(basicAuthInterceptor);
-        registry.addInterceptor(representationAuthInterceptor);
+        registry.addInterceptor(basicAuthInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/login");
     }
 
     @Override
--- backend/agenda/src/main/java/tcp/project/agenda/auth/ui/BasicAuthInterceptor.java
+++ backend/agenda/src/main/java/tcp/project/agenda/auth/ui/BasicAuthInterceptor.java
@@ -1,28 +1,21 @@
-package tcp.project.agenda.auth.ui.interceptor;
+package tcp.project.agenda.auth.ui;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import tcp.project.agenda.auth.exception.MemberNotFoundException;
-import tcp.project.agenda.auth.exception.NotAuthorizedMemberException;
 import tcp.project.agenda.auth.infrastructure.AuthorizationExtractor;
 import tcp.project.agenda.auth.infrastructure.JwtTokenProvider;
-import tcp.project.agenda.member.domain.Member;
-import tcp.project.agenda.member.domain.MemberRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import static tcp.project.agenda.auth.ui.AuthenticationArgumentResolver.AUTHENTICATED_KEY;
-import static tcp.project.agenda.member.domain.Grade.GENERAL;
-import static tcp.project.agenda.member.domain.Grade.REPRESENTATIVE;
 
 @Component
 @RequiredArgsConstructor
-public class RepresentationAuthInterceptor implements HandlerInterceptor {
+public class BasicAuthInterceptor implements HandlerInterceptor {
 
-    private final MemberRepository memberRepository;
     private final JwtTokenProvider tokenProvider;
 
     @Override
@@ -32,21 +25,7 @@ public class RepresentationAuthInterceptor implements HandlerInterceptor {
         String accessToken = AuthorizationExtractor.extract(authHeader);
         Long memberId = tokenProvider.getMemberId(accessToken);
 
-        Member member = getMember(memberId);
-        validateMemberGrade(member);
-
         request.setAttribute(AUTHENTICATED_KEY, memberId);
         return true;
-    }
-
-    private Member getMember(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new MemberNotFoundException(String.valueOf(memberId)));
-    }
-
-    private void validateMemberGrade(Member member) {
-        if (!REPRESENTATIVE.equals(member.getGrade())) {
-            throw new NotAuthorizedMemberException();
-        }
     }
 }
