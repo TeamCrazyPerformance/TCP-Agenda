3e24ab6a2ad94233d535881ca53f8535ded58dac
wlsh44 <k87913j@gmail.com>
2023-03-05 15:46:42 +0900

    fix: 토큰 만료 되었을 때 관련 수정

---
+++ backend/agenda/src/main/java/tcp/project/agenda/common/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package tcp.project.agenda.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String errorMsg;
+}
