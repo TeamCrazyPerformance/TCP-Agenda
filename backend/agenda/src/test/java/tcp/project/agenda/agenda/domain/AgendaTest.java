61afd18b81e436c7be38d1a213368c8e74644003
wlsh44 <k87913j@gmail.com>
2023-03-06 11:25:32 +0900

    test: 검증 기능 테스트 코드 추가

--- backend/agenda/src/test/java/tcp/project/agenda/agenda/domain/AgendaTest.java
+++ backend/agenda/src/test/java/tcp/project/agenda/agenda/domain/AgendaTest.java
@@ -1,6 +1,5 @@
 package tcp.project.agenda.agenda.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,30 +8,30 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import tcp.project.agenda.agenda.application.dto.AgendaCreateRequest;
 import tcp.project.agenda.agenda.exception.InvalidClosedAgendaTimeException;
 import tcp.project.agenda.agenda.exception.InvalidTitleException;
+import tcp.project.agenda.agenda.exception.NotMemberAgendaException;
 import tcp.project.agenda.member.domain.Grade;
 import tcp.project.agenda.member.domain.Member;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.BDDMockito.given;
+import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_CLOSED_AT;
+import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_CONTENT;
+import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_TITLE;
 import static tcp.project.agenda.common.fixture.AgendaFixture.getBasicAgendaCreateRequest;
 import static tcp.project.agenda.common.fixture.AgendaFixture.getInvalidClosedAtAgendaCreateRequest;
 import static tcp.project.agenda.common.fixture.AgendaFixture.getNoTitleAgendaCreateRequest;
-import static tcp.project.agenda.common.fixture.MemberFixture.getExecutiveMember;
 
 @ExtendWith(MockitoExtension.class)
 class AgendaTest {
 
+    @Mock
     Member member;
 
     @Mock
     Grade grade;
-
-    @BeforeEach
-    void init() {
-        member = getExecutiveMember();
-    }
 
     @Test
     @DisplayName("content가 null일 경우 빈 문자열이 저장되어야 함")
@@ -72,4 +71,32 @@ class AgendaTest {
         assertThatThrownBy(() -> Agenda.createAgendaFrom(member, request.getTitle(), request.getContent(), grade, request.getClosedAt()))
                 .isInstanceOf(InvalidClosedAgendaTimeException.class);
     }
+
+    @Test
+    @DisplayName("안건 작성자일 경우 예외가 발생하면 안 됨")
+    void validateAgendaOwner() throws Exception {
+        //given
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, grade, BASIC_AGENDA_CLOSED_AT);
+        Long memberId = 1L;
+        given(member.getId()).willReturn(memberId);
+
+        //when then
+        assertThatNoException()
+                .isThrownBy(() -> agenda.validateOwner(memberId));
+    }
+
+    @Test
+    @DisplayName("안건 작성자가 아닐 경우 예외가 발생해야 함")
+    void validateAgendaOwner_exception() throws Exception {
+        //given
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, grade, BASIC_AGENDA_CLOSED_AT);
+        given(member.getId()).willReturn(1L);
+        Long notMemberId = 999L;
+
+        //when then
+        assertThatThrownBy(() -> agenda.validateOwner(notMemberId))
+            .isInstanceOf(NotMemberAgendaException.class);
+    }
+
+
 }
