649e35f87d01eabc3f762845cb517e19315d1590
astin <wonhun1225@gmail.com>
2023-03-25 18:16:12 +0900

    make input

---
+++ frontend/src/components/basic/input.tsx
@@ -0,0 +1,11 @@
+import styles from './input.scss';
+
+function Input() {
+  return (
+    <div>
+      <input className={styles.input} placeholder="" />
+    </div>
+  );
+}
+
+export default Input;
