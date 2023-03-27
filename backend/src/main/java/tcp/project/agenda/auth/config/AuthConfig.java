571633dc6d4d078a57d868c1edf82ba7612a205a
JinHo Kim <k87913j@gmail.com>
2023-03-27 18:28:38 +0900

    feat: cors 설정 메서드 추가 (#49)
    feat: cors 설정 메서드 추가

--- backend/src/main/java/tcp/project/agenda/auth/config/AuthConfig.java
+++ backend/src/main/java/tcp/project/agenda/auth/config/AuthConfig.java
@@ -3,6 +3,7 @@ package tcp.project.agenda.auth.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tcp.project.agenda.auth.ui.AuthenticationArgumentResolver;
@@ -26,5 +27,11 @@ public class AuthConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthenticationArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 }
