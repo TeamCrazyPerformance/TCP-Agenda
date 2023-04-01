bffb1197a6b78a1cb940b359a3443a210647f557
wlsh44 <k87913j@gmail.com>
2023-04-01 20:54:03 +0900

    test: 투표 생성 테스트 코드 추가

--- backend/src/test/java/tcp/project/agenda/agenda/domain/AgendaTest.java
+++ backend/src/test/java/tcp/project/agenda/agenda/domain/AgendaTest.java
@@ -8,6 +8,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import tcp.project.agenda.agenda.application.dto.AgendaCreateRequest;
 import tcp.project.agenda.agenda.application.dto.AgendaUpdateRequest;
 import tcp.project.agenda.agenda.exception.AgendaAlreadyClosedException;
+import tcp.project.agenda.agenda.exception.AgendaItemNotFoundException;
 import tcp.project.agenda.agenda.exception.InvalidClosedAgendaTimeException;
 import tcp.project.agenda.agenda.exception.InvalidUpdateAlreadyVoteStartedAgendaException;
 import tcp.project.agenda.agenda.exception.NotAgendaOwnerException;
@@ -17,7 +18,6 @@ import tcp.project.agenda.member.domain.GradeType;
 import tcp.project.agenda.member.domain.Member;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
@@ -29,7 +29,6 @@ import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_CLOSE
 import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_CONTENT;
 import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_ITEM1;
 import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_ITEM2;
-import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_SELECTED_LIST_DTO;
 import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_AGENDA_TITLE;
 import static tcp.project.agenda.common.fixture.AgendaFixture.BASIC_UPDATE_AGENDA_ITEM;
 import static tcp.project.agenda.common.fixture.AgendaFixture.getBasicAgendaCreateRequest;
@@ -37,6 +36,8 @@ import static tcp.project.agenda.common.fixture.AgendaFixture.getBasicVoteNotSta
 import static tcp.project.agenda.common.fixture.AgendaFixture.getBasicVoteStartedAgendaUpdateRequest;
 import static tcp.project.agenda.common.fixture.AgendaFixture.getInvalidClosedAtAgendaCreateRequest;
 import static tcp.project.agenda.common.fixture.AgendaFixture.getInvalidClosedAtAgendaUpdateRequest;
+import static tcp.project.agenda.member.domain.GradeType.GENERAL;
+import static tcp.project.agenda.member.domain.GradeType.REGULAR;
 
 @ExtendWith(MockitoExtension.class)
 class AgendaTest {
@@ -154,26 +155,67 @@ class AgendaTest {
     }
 
     @Test
-    @DisplayName("투표 대상 등급이 포함된 경우 아무 일도 일어나지 않음")
-    void validateIsTargetGradeTest() throws Exception {
+    @DisplayName("안건에 투표를 함")
+    void voteTest() throws Exception {
         //given
-        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, GradeType.REGULAR, BASIC_AGENDA_CLOSED_AT);
-        List<Grade> grades = List.of(new Grade(GradeType.REGULAR), new Grade(GradeType.EXECUTIVE));
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
+        AgendaItem agendaItem1 = mock(AgendaItem.class);
+        AgendaItem agendaItem2 = mock(AgendaItem.class);
+        AgendaItem agendaItem3 = mock(AgendaItem.class);
+        agenda.updateAgendaItems(List.of(agendaItem1, agendaItem2, agendaItem3));
+        given(agendaItem1.getId()).willReturn(1L);
+        given(agendaItem2.getId()).willReturn(2L);
+        given(agendaItem3.getId()).willReturn(3L);
+        given(member.getGrades()).willReturn(List.of(REGULAR));
 
-        //when then
-        assertThatNoException()
-                .isThrownBy(() -> agenda.validateIsTargetGrade(grades));
+        //when
+        List<Vote> vote = agenda.vote(member, List.of(1L, 3L));
+
+        //then
+        assertThat(vote).hasSize(2);
     }
 
     @Test
-    @DisplayName("투표 대상이 아닌 경우 예외가 발생해야 함")
-    void validateIsTargetGradeTest_alreadyClosedException() throws Exception {
+    @DisplayName("마감된 안건에 투표를 하면 예외가 발생해야 함")
+    void voteTest_alreadyClosed() throws Exception {
         //given
-        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, GradeType.REGULAR, BASIC_AGENDA_CLOSED_AT);
-        List<Grade> grades = List.of(new Grade(GradeType.GENERAL), new Grade(GradeType.EXECUTIVE));
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
+        agenda.close();
 
         //when then
-        assertThatThrownBy(() -> agenda.validateIsTargetGrade(grades))
+        assertThatThrownBy(() -> agenda.vote(member, List.of(1L, 3L)))
+                .isInstanceOf(AgendaAlreadyClosedException.class);
+    }
+
+    @Test
+    @DisplayName("안건에 투표한 항목이 없을 경우 예외가 발생해야 함")
+    void voteTest_agendaItemNotFound() throws Exception {
+        //given
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
+        AgendaItem agendaItem1 = mock(AgendaItem.class);
+        AgendaItem agendaItem2 = mock(AgendaItem.class);
+        AgendaItem agendaItem3 = mock(AgendaItem.class);
+        agenda.updateAgendaItems(List.of(agendaItem1, agendaItem2, agendaItem3));
+        given(agendaItem1.getId()).willReturn(1L);
+        given(agendaItem2.getId()).willReturn(2L);
+        given(agendaItem3.getId()).willReturn(3L);
+        given(member.getGrades()).willReturn(List.of(REGULAR));
+
+
+        //when then
+        assertThatThrownBy(() -> agenda.vote(member, List.of(4L)))
+                .isInstanceOf(AgendaItemNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("대상이 아닌 안건에 투표를 하면 예외가 발생해야 함")
+    void voteTest_notTargetMember() throws Exception {
+        //given
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
+        given(member.getGrades()).willReturn(List.of(GENERAL));
+
+        //when then
+        assertThatThrownBy(() -> agenda.vote(member, List.of(1L, 3L)))
                 .isInstanceOf(NotTargetMemberException.class);
     }
 
@@ -181,7 +223,7 @@ class AgendaTest {
     @DisplayName("아직 투표가 시작되지 않은 안건일 경우, '제목, 내용, 마감 시간, 대상'이 바뀔 수 있음")
     void updateTest_voteNotStarted() throws Exception {
         //given
-        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, GradeType.REGULAR, BASIC_AGENDA_CLOSED_AT);
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
         AgendaUpdateRequest request = getBasicVoteNotStartedAgendaUpdateRequest();
 
         //when
@@ -200,7 +242,7 @@ class AgendaTest {
     @DisplayName("마감 시간이 시작 시간보다 빠른 경우 예외가 발생해야 함")
     void updateTest_invalidClosedAt() throws Exception {
         //given
-        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, GradeType.REGULAR, BASIC_AGENDA_CLOSED_AT);
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
         AgendaUpdateRequest request = getInvalidClosedAtAgendaUpdateRequest();
 
         //when then
@@ -212,7 +254,7 @@ class AgendaTest {
     @DisplayName("이미 종료된 안건인 경우 예외가 발생해야 함")
     void updateTest_alreadyClosed() throws Exception {
         //given
-        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, GradeType.REGULAR, BASIC_AGENDA_CLOSED_AT);
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
         agenda.close();
         AgendaUpdateRequest request = getBasicVoteNotStartedAgendaUpdateRequest();
 
@@ -225,9 +267,8 @@ class AgendaTest {
     @DisplayName("투표가 시작된 안건일 경우, '마감 시간'이 바뀔 수 있음")
     void updateTest_voteStarted() throws Exception {
         //given
-        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, GradeType.REGULAR, BASIC_AGENDA_CLOSED_AT);
-        List<AgendaItem> agendaItemList = BASIC_AGENDA_SELECTED_LIST_DTO.stream().map(agendaItemDto -> new AgendaItem(agendaItemDto.getContent())).collect(Collectors.toList());
-        agenda.updateAgendaItems(agendaItemList);
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
+        agenda.updateAgendaItems(List.of(AgendaItem.createAgendaItem(BASIC_AGENDA_ITEM1), AgendaItem.createAgendaItem(BASIC_AGENDA_ITEM2)));
         agenda.addVote(mock(Vote.class));
         AgendaUpdateRequest request = getBasicVoteStartedAgendaUpdateRequest();
 
@@ -240,7 +281,7 @@ class AgendaTest {
                 () -> assertThat(agenda.getTitle()).isEqualTo(BASIC_AGENDA_TITLE),
                 () -> assertThat(agenda.getContent()).isEqualTo(BASIC_AGENDA_CONTENT),
                 () -> assertThat(agenda.getClosedAt()).isEqualTo(request.getClosedAt()),
-                () -> assertThat(agenda.getTarget()).isEqualTo(GradeType.REGULAR),
+                () -> assertThat(agenda.getTarget()).isEqualTo(REGULAR),
                 () -> assertThat(changedAgendaItems).hasSize(2)
         );
     }
@@ -249,9 +290,8 @@ class AgendaTest {
     @DisplayName("이미 투표가 시작된 안건인 경우에 내용, 대상을 수정하려고 하면 예외가 발생해야 함")
     void updateTest_invalidUpdateAlreadyStartedVoteAgenda() throws Exception {
         //given
-        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, GradeType.REGULAR, BASIC_AGENDA_CLOSED_AT);
-        List<AgendaItem> agendaItemList = BASIC_AGENDA_SELECTED_LIST_DTO.stream().map(agendaItemDto -> new AgendaItem(agendaItemDto.getContent())).collect(Collectors.toList());
-        agenda.updateAgendaItems(agendaItemList);
+        Agenda agenda = Agenda.createAgendaFrom(member, BASIC_AGENDA_TITLE, BASIC_AGENDA_CONTENT, REGULAR, BASIC_AGENDA_CLOSED_AT);
+        agenda.updateAgendaItems(List.of(AgendaItem.createAgendaItem(BASIC_AGENDA_ITEM1), AgendaItem.createAgendaItem(BASIC_AGENDA_ITEM2)));
         agenda.addVote(mock(Vote.class));
         AgendaUpdateRequest request = getBasicVoteNotStartedAgendaUpdateRequest();
 
