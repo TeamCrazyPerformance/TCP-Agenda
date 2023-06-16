8e393bbd6941b070b599746156732f8e45fde36e
Donghun Won <wonhun1225@gmail.com>
2023-06-16 15:41:05 +0900

    fix: 버그 수정

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,20 @@
+import Vote from 'components/Vote';
+import AdminVote from 'components/VoteAdmin';
+
 import styles from './index.scss';
+
 function Home() {
+  const data = {
+    id: '1',
+    title: 'test',
+    createdAt: '2023-03-01T22:59:59',
+    closedAt: '2023-03-01T22:59:59',
+    target: '재학생',
+    votedMember: '10',
+    isOpen: false,
+  };
   return (
     <div className={styles.container}>
-      <p>This is /</p>
     </div>
   );
 }
