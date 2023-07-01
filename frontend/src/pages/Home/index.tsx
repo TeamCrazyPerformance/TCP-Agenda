6c234b09c2752b8a2c1fe130a1fe0c14977848fb
Donghun Won <wonhun1225@gmail.com>
2023-07-01 17:17:51 +0900

    feat: vote 컴포넌트 수정 완료

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import Vote from 'components/Vote';
+
 import styles from './index.scss';
 
 function Home() {
@@ -8,10 +10,11 @@ function Home() {
     closedAt: '2023-03-01T22:59:59',
     target: '재학생',
     votedMember: '10',
-    isOpen: true,
+    isOpen: false,
   };
   return (
     <div className={styles.container}>
+      <Vote data={data} />
     </div>
   );
 }
